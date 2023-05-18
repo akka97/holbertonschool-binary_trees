20afa80daf02dbec34773d33f11fea107af4cde6
akka97 <akapellari@yahoo.com>
2023-05-18 03:09:23 -0700

    tasks

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -15,7 +15,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		return (0);
 	count_l = binary_tree_nodes(tree->left);
 	count_r = binary_tree_nodes(tree->right);
-	if (tree->left != NULL && tree->right != NULL)
+	if (tree->left != NULL || tree->right != NULL)
 		return (count_l + count_r + 1);
 	return (count_l + count_r + 1);
 }
